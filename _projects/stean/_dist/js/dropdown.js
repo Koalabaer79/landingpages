92b6068961e39516a2d713f2eb6c33ef0fe98def
Lars Urban <urban@u-php.de>
2021-03-03 08:48:16 +0100

    development

---
+++ _projects/stean/_dist/js/dropdown.js
@@ -0,0 +1,22 @@
+function dropdown() {
+	var x = document.getElementById("navItems");
+	if (x.style.height === "0px" || x.style.height === "" ) {
+		x.style.height = "315px";
+	} else {
+	  	x.style.height = "0px";
+	}
+}
+
+window.addEventListener("resize", resizeFunc);
+
+function resizeFunc() {
+	el = document.getElementById('navItems');
+	width = window.innerWidth;
+	if(width <= 700) {
+		console.log(width)
+		el.style.height = "0px";
+	}else{
+		console.log(width)
+		el.style.height = "50px"
+	}
+}
